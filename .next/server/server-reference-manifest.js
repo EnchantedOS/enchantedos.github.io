3c25e7c17f9dcc38a9fae57835885d77648eb23d
Janis Jendraß <me@janis.io>
2024-04-07 19:46:41 +0200

    Static export

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"VlgSEH1FN5xvPqk/2hsQIpRJtNeibIxeCxyejzxarAE=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"cesswADYICKJd9YKzLl/bfooc7S1dfZqfEJj+XOyZig=\"}"
---
+++ out/_next/static/chunks/app/authentication/page-e356c8e81126add9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[940,720,613,195,517,750,734,450,297,586],{6225:function(e,n,s){Promise.resolve().then(s.t.bind(s,5250,23)),Promise.resolve().then(s.bind(s,3801)),Promise.resolve().then(s.bind(s,4052)),Promise.resolve().then(s.bind(s,5824))}},function(e){e.O(0,[250,371,841,971,69,744],function(){return e(e.s=6225)}),_N_E=e.O()}]);
